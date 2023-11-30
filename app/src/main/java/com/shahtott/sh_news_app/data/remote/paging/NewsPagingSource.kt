da987f479973a16a84edae6f4c26ac14df564796
Elshahatt <MahmoudElshahatt>
2023-11-30 14:17:06 -0800

    make a news paging source

--- app/src/main/java/com/shahtott/sh_news_app/data/remote/paging/NewsPagingSource.kt
+++ app/src/main/java/com/shahtott/sh_news_app/data/remote/paging/NewsPagingSource.kt
@@ -9,22 +9,36 @@ import com.shahtott.sh_news_app.ui.util.Constants
 class NewsPagingSource(
     private val newsApi: NewsApi,
     private val sources: String,
-) : PagingSource<Int, Article> {
-    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+) : PagingSource<Int, Article>() {
+    private var totalNewsCount = 0
 
-    }
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
         val page = params.key ?: 1
         return try {
             val newsResponse =
                 newsApi.getNews(sources = sources, page = page)
-
+            totalNewsCount += newsResponse.articles?.size ?: 0
+            val articles =
+                newsResponse.articles?.distinctBy { it?.title }?.filterNotNull()
+                    ?: emptyList()  //distinctBy to remove duplicates.
+            LoadResult.Page(
+                data = articles,
+                nextKey = if (totalNewsCount == newsResponse.totalResults) null else page + 1,
+                prevKey = null
+            )
         } catch (e: Exception) {
             e.printStackTrace()
             LoadResult.Error(
                 e
             )
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
         }
     }
 }
